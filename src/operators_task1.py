d0248df02c641e99d235cbd4f5ff8df992be760b
Santosh.Devarshetti <santosh.abap@gmail.com>
2025-10-14 12:06:31 +0530

    Commiting task files

---
+++ src/operators_task1.py
@@ -0,0 +1,4 @@
+base = float(input("please enter the base of Triangle"))
+height = float(input("please enter the height of Triangle"))
+area = 0.5* base * height
+print("area of triangle is ", area)
---
+++ src/operators_task2.py
@@ -0,0 +1,5 @@
+userno = int(input("please enter the no of users:"))
+if userno == 100:
+    print("you have entered 100 ")
+else:
+    print("no is not equal to 100")
