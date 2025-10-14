1ed666c0ec65b5dcf0fadc01d551015095987ebc
Santosh Devarshetti <santosh.abap@gmail.com>
2025-10-14 13:57:49 +0530

    Commiting the latest  task files

---
+++ src/Lab004_Basics.py
@@ -0,0 +1,14 @@
+a,b,c,d = 10,20,30,40
+print(a,b,c,d,sep="|")
+result = max(a,b,c,d)
+print(result)
+print( a + 99)
+print("hello" + str(99))
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+result = num1 + num2
+print(result)g
+result = num1 * num2
+print(result)
+result = num1/num2
+print(result)
